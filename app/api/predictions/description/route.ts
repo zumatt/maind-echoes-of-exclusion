8ec840a6579a420175dbaa42bc8b6e92ddf27689
Matteo Subet <hi@zumat.ch>
2025-09-11 09:47:31 +0200

    Added tokens to description

--- app/api/predictions/description/route.ts
+++ app/api/predictions/description/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: Request) {
   const input = {
     image: imageUrl,
     prompt: 'Describe this image with a short but precise description.',
-    max_tokens: 60,
+    max_tokens: 80,
   };
 
   const prediction = await replicate.predictions.create({
